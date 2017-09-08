1e983c52b7fd5ccdc17ab9886ec8b3ef3bfde81f
unknown <459676797@qq.com>
2017-09-08 18:02:23 +0800

    some change

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ if (window['context'] === undefined) {
   }
   window['context'] = location.origin + '/V6.0'
 }
-axios.defaults.baseURL = window.location.origin + '/haimentest/'
+axios.defaults.baseURL = window.location.origin + '/haimen/'
 Vue.prototype.$baseUrl = window.location.origin + window.location.pathname
 axios.interceptors.request.use(function (config) {
   if (cookie.getCookie('token')) {
